8dfa390d7f60ae1d5b4274bff990e8b70a6c4b1a
Raghavendra Gowda <rraghavendraprasadgowda@gmail.com>
2020-03-22 22:46:32 +0530

    Eclipse Commit

--- src/test/java/Jenkins_Tester/JTesting/Jen.java
+++ src/test/java/Jenkins_Tester/JTesting/Jen.java
@@ -1,11 +1,25 @@
 package Jenkins_Tester.JTesting;
 
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
+
+
+
 public class Jen {
+	
+	
+	public static Logger ls=LogManager.getLogger(Jen.class.getName());
 
 	@Test
 	public static void Tes() {
 		System.out.println("**************************Testing to Check Maven and Jenkins Integration*************************************");
+	    ls.info("Test Verified");
+	
 	}
+
+
 }
