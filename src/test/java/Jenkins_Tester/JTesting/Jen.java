0bb7d9cdcc8c1ac4d62d4f4299db50512c9e5c1c
Raghavendra Gowda <rraghavendraprasadgowda@gmail.com>
2020-03-22 20:53:21 +0530

    JenMavTest

---
+++ src/test/java/Jenkins_Tester/JTesting/Jen.java
@@ -0,0 +1,11 @@
+package Jenkins_Tester.JTesting;
+
+import org.testng.annotations.Test;
+
+public class Jen {
+
+	@Test
+	public static void Tes() {
+		System.out.println("**************************Testing to Check Maven and Jenkins Integration*************************************");
+	}
+}
